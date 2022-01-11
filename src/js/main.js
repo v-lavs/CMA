402d27af9a3618e954ead7d8fe31a7736632e389
v-lavs <lavskavanda@gmail.com>
2022-01-11 23:05:56 +0200

    add layot and assets, js

---
+++ src/js/main.js
@@ -0,0 +1,121 @@
+/*
+* to include js file write: `//= include ./path-to-file`
+* */
+
+//= include ../../node_modules/jquery/dist/jquery.js ;
+//= include ../../node_modules/waypoints/lib/jquery.waypoints.min.js ;
+
+
+// CUSTOM SCRIPTS
+
+$(document).ready(function () {
+
+    //MOBILE MENU
+    const nav = $('.header__nav');
+
+    $('.btn_burger').click(function (e) {
+        e.preventDefault();
+        nav.addClass('open');
+        jQuery('.backdrop').fadeIn();
+    });
+
+    $('.btn_close, .backdrop').click(function (e) {
+        e.preventDefault();
+        nav.removeClass('open');
+        jQuery('.backdrop').fadeOut();
+    });
+
+    // SMOOTH SCROLL TO ANCHOR
+    function smoothScrollToAnchor(selector) {
+        $(selector).on('click', function (event) {
+            let anchor = $.attr(this, 'href')
+
+            if (anchor.match(/^#/) && anchor !== '#') {
+                event.preventDefault();
+
+                $('html, body').animate({
+                    scrollTop: $($.attr(this, 'href')).offset().top
+                }, 1000);
+                nav.removeClass('open');
+                jQuery('.backdrop').fadeOut();
+                $('body').removeClass('modal_open');
+            }
+        })
+
+    }
+
+    smoothScrollToAnchor('.btn_scroll');
+    smoothScrollToAnchor('.menu__link');
+
+    //SWIPER-SLIDER
+    const sliderActivity = new Swiper(".slider-activity", {
+        spaceBetween:5,
+        slidesPerView: "auto",
+        loop:true,
+        speed: 1500,
+        on: {
+            afterInit: function () {
+                const currSlide = this.slides[this.activeIndex];
+                showActivityText(currSlide);
+            }
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable:true,
+        },
+    });
+    sliderActivity.on('slideChange', function () {
+        const currSlide = this.slides[this.activeIndex];
+        showActivityText(currSlide);
+    });
+
+
+    function showActivityText(currSlide) {
+        const textHolder = $('#slideText');
+        if (textHolder && textHolder.length > 0) {
+            const text = $(currSlide).find('.text-template').html();
+            textHolder.html(text);
+        }
+    }
+
+
+    // MODAL
+    //
+    // $('.tabs__link').on('click', function (e) {
+    //     $('.tabs__link').removeClass('active');
+    //     $(this).addClass('active');
+    //
+    // });
+    //
+    // $('.modal-toggle').on('click', function (e) {
+    //     e.preventDefault();
+    //     const modalID = $(this).data('modal');
+    //     console.log(this);
+    //     const modal = $(modalID).addClass('active');
+    //     jQuery('.backdrop').fadeIn();
+    //     $("body").addClass("modal-open");
+    // });
+    //
+    // $('.modal__close, .backdrop').on('click', function (e) {
+    //     e.preventDefault();
+    //     $('.modal').removeClass('active');
+    //     jQuery('.backdrop').fadeOut();
+    //     $("body").removeClass("modal-open");
+    // });
+    //
+    //
+    // //TABS
+    // const tabLink = $('.tabs__link');
+    // const tabContentItem = $('.tab');
+    // tabLink.click(function (e) {
+    //     e.preventDefault();
+    //     tabLink.removeClass('active');
+    //     tabContentItem.removeClass('active');
+    //     $(e.target).addClass('active');
+    //     $($(e.currentTarget).attr('href')).addClass('active');
+    // });
+
+});
+
+
+
